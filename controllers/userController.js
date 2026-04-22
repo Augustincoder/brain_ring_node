e0ecc67d19f83689eb1d8862b8f03bc0485cafad
Muhammadamin <augustin.coder@gmail.com>
2026-04-23 02:05:09 +0500

    fixed errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,12 +11,17 @@ const User = require('../models/User');
  */
 const getMyProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id)
-      .select('-password -playedQuestions')
-      .lean();  // lean for fast read-only response
-
+    const user = await User.findById(req.user._id).select('-password -playedQuestions');
+    
     if (!user) {
       return res.status(404).json({ success: false, message: 'User not found.' });
+    }
+
+    const prevStreak = user.currentStreak;
+    user.recalculateStreak();
+    
+    if (user.currentStreak !== prevStreak) {
+      await user.save();
     }
 
     res.status(200).json({ success: true, data: user });
